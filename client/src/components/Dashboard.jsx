179970abd939f03f2fce4f6bdfc58c7c378da48c
lonemohsin <lonemohsin4@gmail.com>
2023-05-01 13:21:39 +0530

    laundaryf

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,45 +1,83 @@
-import { Box, Flex, Grid, GridItem, Heading, Wrap } from '@chakra-ui/react';
+import { Box, Flex, Grid, GridItem, Heading, Wrap, useBreakpointValue } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react'
 
 const Dashboard = () => {
+  const isMobile = useBreakpointValue({ base: true, md: false });
     const [data, setData] = useState([])
 
     useEffect(() => {
         getData(); 
     }, [])
     async function getData() {
-        const data = await fetch("http://localhost:3000/dashboard")
+        const data = await fetch(
+          "https://laundary-epml.onrender.com/dashboard"
+        );
         const res = await data.json()
         setData(res.message)
-        console.log(res)
+      
     }
     
   return (
-    <Flex flexDirection={"column"} alignItems={"center"} gap={10}>
-      <Box>
-        <Heading children="New requests" />
-      </Box>
-      <Wrap>
-        <Box borderWidth="1px" borderRadius="lg" overflow="hidden" p="4">
-          <Grid templateColumns="repeat(5, 1fr)" gap="6">
-            <GridItem fontWeight="bold">Name</GridItem>
-            <GridItem fontWeight="bold">Shirt</GridItem>
-            <GridItem fontWeight="bold">Phone</GridItem>
-            <GridItem fontWeight="bold">Address</GridItem>
-            <GridItem fontWeight="bold">Date</GridItem>
-            {data.map((item, index) => (
-              <>
-                <GridItem>{item.name}</GridItem>
-                <GridItem>{item.shirt}</GridItem>
-                <GridItem>{item.phone}</GridItem>
-                <GridItem>{item.address}</GridItem>
-                <GridItem>{item.date}</GridItem>
-              </>
-            ))}
-          </Grid>
-        </Box>
-      </Wrap>
-    </Flex>
+    <>
+      {isMobile ? (
+        <Flex
+          width={"100%"}
+          flexDirection={"column"}
+          alignItems={"center"}
+          gap={10}
+        >
+          <Box>
+            <Heading children="New requests" />
+          </Box>
+          <Wrap width={"80%"}>
+            <Box borderWidth="1px" borderRadius="lg" overflow="scroll" p="4">
+              <Grid templateColumns="repeat(5, 1fr)" gap="6">
+                <GridItem fontWeight="bold">Name</GridItem>
+                <GridItem fontWeight="bold">Shirt</GridItem>
+                <GridItem fontWeight="bold">Phone</GridItem>
+                <GridItem fontWeight="bold">Address</GridItem>
+                <GridItem fontWeight="bold">Date</GridItem>
+                {data.map((item, index) => (
+                  < >
+                    <GridItem >{item.name}</GridItem>
+                    <GridItem >{item.shirt}</GridItem>
+                    <GridItem >{item.phone}</GridItem>
+                    <GridItem >{item.address}</GridItem>
+                    <GridItem >{item.date}</GridItem>
+                  </>
+                ))}
+              </Grid>
+            </Box>
+          </Wrap>
+        </Flex>
+      ) : (
+        <Flex flexDirection={"column"} alignItems={"center"} gap={10}>
+          <Box>
+            <Heading children="New requests" />
+          </Box>
+          <Wrap>
+            <Box borderWidth="1px" borderRadius="lg" overflow="hidden" p="4">
+              <Grid templateColumns="repeat(5, 1fr)" gap="6">
+                <GridItem fontWeight="bold">Name</GridItem>
+                <GridItem fontWeight="bold">Shirt</GridItem>
+                <GridItem fontWeight="bold">Phone</GridItem>
+                <GridItem fontWeight="bold">Address</GridItem>
+                <GridItem fontWeight="bold">Date</GridItem>
+                {data.map((item, index) => (
+                  <>
+                    <GridItem>{item.name}</GridItem>
+                    <GridItem>{item.shirt}</GridItem>
+                    <GridItem>{item.phone}</GridItem>
+                    <GridItem>{item.address}</GridItem>
+                    <GridItem>{item.date}</GridItem>
+                  </>
+                ))}
+              </Grid>
+            </Box>
+          </Wrap>
+        </Flex>
+      )}
+    </>
   );
 }
 
--- client/src/components/Topcontainer.jsx
+++ client/src/components/Topcontainer.jsx
@@ -51,7 +51,7 @@ const Topcontainer = () => {
     formData.address = address
     formData.date = new Date().toLocaleString();
     
-    const res = await fetch("http://localhost:3000/submit", {
+    const res = await fetch("https://laundary-epml.onrender.com/submit", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
